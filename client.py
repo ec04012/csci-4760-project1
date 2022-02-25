29c4348e77553eb7f58144ab1fc6c97bcd247497
ec04012 <ec04012@uga.edu>
2022-02-24 20:25:54 -0500

    V1.1 Working version

--- client.py
+++ client.py
@@ -5,11 +5,15 @@ c_soc = socket(AF_INET, SOCK_STREAM)
 c_soc.connect((s_name, s_port))
 print('Connected to Remote Server')
 
-data = input("input lowercase sentence ")
+data = input("Enter a Name\n")
 c_soc.send(data.encode())
+print("")
+
+data = input("Enter an integer between 1 and 100\n")
+c_soc.send(data.encode())
+print("")
+
 print('Data sent... waiting for the response.')
-
 resp = c_soc.recv(1024)
-print('From Server', resp.decode())
+print('Server', resp.decode())
 c_soc.close()
-
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from socket import *
-
+from random import randint
 
 port = 12000
 s_soc = socket(AF_INET, SOCK_STREAM)
@@ -7,13 +7,43 @@ s_soc.bind(('localhost', port))
 s_soc.listen(1)
 ServerName = "Universal Server"
 print(ServerName + " is now ONLINE")
+print("")
 
+# server returns a string containing (name, random number, sum)
 while True:
-
         con, addr = s_soc.accept()
-        data = con.recv(1024).decode()
+        
+        # Get name
+        inputName = con.recv(1024).decode()
 
-        resp = data.upper()
-        con.send(resp.encode())
-        print("Sent")
-        con.close()
+        # Get number
+        inputNum = con.recv(1024).decode()        
+        try:
+                # convert to int
+                inputNum = int(inputNum)
+                # check if input is in bounds
+                if inputNum < 1 or inputNum > 100:
+                        raise Exception("Number out of range")
+        except:
+                # if error, send error message in response
+                print("Error: Client entered an invalid number")
+                print("")
+                resp = "Error: Please input a valid number"
+        else:
+                # if no error
+                # Generate random number
+                randNum = randint(1,100)
+                
+                # Create proper response
+                resp = "Server of %s, Random Int %i, Sum %i" % (inputName, randNum, inputNum+randNum)
+                print("")
+                print("   Server of: " + inputName)
+                print("Random value: " + str(randNum))
+                print("         Sum: " + str(inputNum+randNum))
+                print("")
+                print("Sent response")
+                print("")
+        finally:
+                # send response
+                con.send(resp.encode())
+        #con.close()
